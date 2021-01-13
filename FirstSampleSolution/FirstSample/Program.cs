a7fdd32d497de05cb1fa13475410b6af2ac8f711
jcha1nait <77072999+jcha1nait@users.noreply.github.com>
2021-01-13 10:01:18 -0700

    FirstSample+Solution

---
+++ FirstSampleSolution/FirstSample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FirstSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //the WriteLine() command will output the string
+            //to the output window.
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This course section is: \t OA3");
+            Console.WriteLine("The instructors name is: \t Don Welch");
+        }
+    }
+}
