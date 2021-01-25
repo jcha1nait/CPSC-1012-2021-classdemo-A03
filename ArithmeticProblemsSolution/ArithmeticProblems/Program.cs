e64483360c66fe8782b71783afc80b6efc2794c2
jcha1nait <77072999+jcha1nait@users.noreply.github.com>
2021-01-24 17:10:11 -0700

    Basic Programming Problems

---
+++ ArithmeticProblemsSolution/ArithmeticProblems/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace ArithmeticProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calculate mean average
+            //declare 4 double variables
+            //declare 1 string variable for input
+            //prompt, input and convert number; repeat 3 times
+            //add numbers together and divide by 3
+            //display mean average
+
+            double numberOne;
+            double numberTwo;
+            double numberThree;
+            double meanAverage;
+
+            //Could technically code the above 4 lines on one physical line
+            //double numberOne, numberTwo, numberThree, meanAverage
+            string inputNumber;
+            Console.Write("Enter a real number:\t");
+            inputNumber = Console.ReadLine();
+            numberOne = double.Parse(inputNumber);
+            Console.Write("Enter a real number:\t");
+            inputNumber = Console.ReadLine();
+            numberTwo = double.Parse(inputNumber);
+            Console.Write("Enter a real number:\t");
+            inputNumber = Console.ReadLine();
+            numberThree = double.Parse(inputNumber);
+            meanAverage = (numberOne + numberTwo + numberThree) / 3.0;
+            Console.WriteLine($"{numberOne} + {numberTwo} + {numberThree} has an average of {meanAverage}.");
+            
+            //Practice Math.Round()
+            Console.WriteLine($"{numberOne} + {numberTwo} + {numberThree}" +
+                $"has an average of of {Math.Round(meanAverage, 2)}");
+
+
+            //Pythagorean Theorem
+            //declare 3 double variables
+            //declare 1 string variable for reuse as input variable
+            //prompt and read number for height
+            //prompt and read number for base
+            //calculate hypotenuse (use Math class methods)
+            //formula hypotenuse = squareroot(height squared + base squared)
+            //display data
+
+            double triangleHeight;
+            double triangleBase;
+            double hypotenuse;
+            Console.Write("enter the triangle''s height;\t");
+           
+            //declare and assign a value to a variable
+            string inputTriangle = Console.ReadLine();
+            triangleHeight = double.Parse(inputTriangle);
+            Console.Write("enter the triangle''s base;\t");
+
+            //reuse a declared variable and reassign a value to that variable
+            inputTriangle = Console.ReadLine();
+
+            triangleBase = double.Parse(inputTriangle);
+
+            //Math.Sqrt() calculates square root value
+            //Math.Pow() calculates a value raised to a specific power
+            hypotenuse = Math.Sqrt(Math.Pow(triangleHeight, 2) + Math.Pow(triangleBase, 2));
+            Console.WriteLine($"The hypotenuse of a triangle with a height of {triangleHeight}" +
+                $" and a base of {triangleBase} is {hypotenuse}");
+
+            //Sum Numbers
+
+            //declare 2 integer variable
+            //declare 1 string variable for input
+            //prompt, input and convert number
+            //divide number by 100 and add to sum integer variable
+            //get remainder of number divided by 100, divide remainder by 10, add to sum integer
+            //get remainder of number divided by 10, add to sum integer 
+            //display three numbers and sum
+
+            int inputNumberTemp;
+            int totalSum = 0;
+            string inputSumNumber;
+
+            Console.Write("Enter a whole number:\t");
+            inputSumNumber = Console.ReadLine();
+            inputNumberTemp = int.Parse(inputSumNumber);
+
+            //Modulas division to get remainder
+            //345 & 100 -> 45 -> 45/10 -> 4
+            totalSum += inputNumberTemp / 100;
+
+            totalSum += (inputNumberTemp % 100) / 10;
+
+            //Modulas division to get remainder
+            //345%100 -> 45 -> 45%10 -> 5
+            totalSum += (inputNumberTemp % 100) % 10;
+
+            Console.WriteLine($"The digits of {inputNumberTemp} sum to {totalSum}");
+
+
+        }
+    }
+}
---
+++ BasicProgrammingProblemsSolution/BasicProgrammingProblems/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace BasicProgrammingProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //West Coast Sales
+            //declare 3 double variables
+            //Assign value to SalesUnitPercentage
+            //Asiggn value to totalSales
+            //Calculate totalSalesofSalesUnits = totalSales * SalesUnitPercentage / 100
+            //Display calculated value
+
+            double SalesUnitPercentage = 43.0;
+            double totalSales = 5.3;
+            double totalSalesofSalesUnits;
+
+            totalSalesofSalesUnits = (totalSales * SalesUnitPercentage) / 100;
+
+            Console.WriteLine($"The West Coast sales unit generated {Math.Round(totalSalesofSalesUnits,2)} million in revenue this year.");
+
+            //Land Calculator
+            //declare 4 double variables
+            //declare string variable for user input
+            //Prompt and read value for user input
+            //Convert user input value into double datatype
+            //Calculate landvalue = AcreSize/AcretoFeet * feetValue
+            //display calculated value
+
+            double AcreSize = 1.0;
+            double AcretoFeet = 43560.0;
+            double feetValue;
+            double landValue;
+
+            string inputFeet;
+            Console.Write("How big is the size of the land in square feet?\t");
+            inputFeet = Console.ReadLine();
+
+            feetValue = double.Parse(inputFeet);
+
+            landValue = (AcreSize / AcretoFeet) * feetValue;
+
+            Console.WriteLine($"The size of the land of {feetValue} square feet in acres is {Math.Round(landValue,2)} acres.");
+
+            //Calories Consumed
+            //declare 3 int variables and 4 double variables
+            //Calculate totalCalories = ServingsValue * CalorieValue
+            //declare string value for user input
+            //Prompt and read value for user input
+            //Convert user input value into double datatype
+            //Calculate servingsEaten = cookiesEaten*ServingsValue / TotalCookies
+            //Calculate caloriesConsumed = servingsEaten*totalCalories / ServingsValue
+            //use a two way if condition statement to display calculated value depending on the value of user input
+            int TotalCookies = 26;
+            int ServingsValue = 10;
+            int CalorieValue = 175;
+            double cookiesEaten;
+            double servingsEaten;
+            double caloriesConsumed;
+            double totalCalories = ServingsValue * CalorieValue;
+
+            string inputCookies;
+            Console.Write("How many cookies have you eaten?\t");
+            inputCookies = Console.ReadLine();
+
+            cookiesEaten = double.Parse(inputCookies);
+
+            servingsEaten = (cookiesEaten * ServingsValue) / TotalCookies;
+            caloriesConsumed = (servingsEaten * totalCalories) / ServingsValue;
+
+            if (cookiesEaten >= 0)
+            {
+                if (cookiesEaten == 1)
+                {
+                    Console.WriteLine($"The amount of calories consumed if you ate {cookiesEaten} cookie is {caloriesConsumed} calories.");
+                }
+                else
+                {
+                    Console.WriteLine($"The amount of calories consumed if you ate {cookiesEaten} cookies is {caloriesConsumed} calories.");
+                }
+            }
+
+            
+
+
+
+        }
+    }
+}
