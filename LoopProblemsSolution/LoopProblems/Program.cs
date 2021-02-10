e623cc1463c75a17fc5688d9fa8b3ac7d7bfc741
jcha1nait <77072999+jcha1nait@users.noreply.github.com>
2021-02-10 11:32:05 -0700

    Loop Problems Program

--- LoopProblemsSolution/LoopProblems/Program.cs
+++ LoopProblemsSolution/LoopProblems/Program.cs
@@ -7,9 +7,9 @@ namespace LoopProblems
         static void Main(string[] args)
         {
             //1.
-            string inputTemp;
-            int mySquare = 0;
-            int sumSquares = 0;
+            //string inputTemp;
+            //int mySquare = 0;
+            //int sumSquares = 0;
 
 
             //Console.WriteLine("Please enter a number and find out its sum of squares:\t");
@@ -69,27 +69,128 @@ namespace LoopProblems
             //5
             //4
             //3
-            Console.Write("Enter a number and display digits in reverse order:\t");
-            inputTemp = Console.ReadLine();
-            int aNumber;
-            if (int.TryParse(inputTemp,out aNumber))
-            {
-                int aNumberLength = inputTemp.Length;
+            //    Console.Write("Enter a number and display digits in reverse order:\t");
+            //    inputTemp = Console.ReadLine();
+            //    int aNumber;
+            //    if (int.TryParse(inputTemp,out aNumber))
+            //    {
+            //        int aNumberLength = inputTemp.Length;
 
-                //use the for{...} loop syntax
-                //the for loop is a pre-test structure
-                //for(declare loop counter; end condition; increment/decrement)
-                //{
-                //  coding block
-                //}
-                for (int loopCounter = aNumberLength - 1; loopCounter >= 0; loopCounter--)
-                {
-                    Console.WriteLine($"{inputTemp.Substring(loopCounter, 1)}");
-                }
-            }
-            else
+            //        //use the for{...} loop syntax
+            //        //the for loop is a pre-test structure
+            //        //for(declare loop counter; end condition; increment/decrement)
+            //        //{
+            //        //  coding block
+            //        //}
+            //        for (int loopCounter = aNumberLength - 1; loopCounter >= 0; loopCounter--)
+            //        {
+            //            Console.WriteLine($"{inputTemp.Substring(loopCounter, 1)}");
+            //        }
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"{inputTemp} is invalid. Enter a number.");
+            //    }
+
+            //MoneyMaker
+            //This program will accept a principal investment amoun, a monthly interest rate,
+            //and a investment time in months.
+            //This program will display a monthly investment report using the incoming data
+            //This program will continue until the user enters an x to exit.
+
+            //process: declare/assignment starting variables
+            //         request execution or exit from the user
+            //              on exit terminate program
+            //              on execution
+            //                  input investment values
+            //                  iternate monthly earnings and report
+            //                  on termination of investment period, display a summary
+
+            //this example is one of using nested loops
+            //menu is a good candidate for a post-test loop
+            //investment loop is a good candidate for a pre-test loop using a for() loop
+            //output will demonstrate formatting of values and columns
+
+            decimal myPrincipal = 0.0m; //the m "types" this numeric as a decimal
+            decimal myMonthlyInterestRate = 0.0m;
+            Int32 myInvestmentTime = 0; //Int32 is equivalent to int
+
+            string menuChoice = "";
+
+            do
             {
-                Console.WriteLine($"{inputTemp} is invalid. Enter a number.");
+                Console.WriteLine("Welcome to CPSC Investments:\n\n");
+                Console.WriteLine("a) investment:");
+                Console.WriteLine("x) to exit\n");
+                Console.Write("Enter your menu choice:\t");
+                menuChoice = Console.ReadLine();
+
+                switch(menuChoice.ToUpper())
+                {
+                    case "A":
+                        {
+                            //for this example, I will assume valid data is entered.
+                            Console.Write("\nEnter your principal investment amount:\t");
+                            string inputTemp = Console.ReadLine();
+                            myPrincipal = decimal.Parse(inputTemp);
+                            Console.Write("\nEnter your investment monthly rate (3% -> 0.03):\t");
+                            inputTemp = Console.ReadLine();
+                            myMonthlyInterestRate = decimal.Parse(inputTemp);
+                            Console.Write("\nEnter your investment period in months:\t");
+                            inputTemp = Console.ReadLine();
+                            myInvestmentTime = Int32.Parse(inputTemp);
+
+                            //use a loop for a fixed amount of iterations
+                            //best candidate would be a pre-test loop
+                            // a) While with a counter
+                            // b) for (...) loops
+                            for (int counter = 0; counter < myInvestmentTime; counter++)
+                            {
+                                // the {0} is referred to as a placeholder
+                                // the string.Format(format pattern, value for the pattern)
+                                // the pattern in this case is currency and the 0 indicates a placeholder for the value
+                                // in the .Format method
+                                Console.WriteLine("\nOpening; {0}", string.Format("{0:c}", myPrincipal));
+
+                                //.ToString("pattern")
+                                //# indicates a digit position and is optional, printed if NOT zero (0)
+                                //0 indicates a digit position and is required, zeroes are printed
+                                Console.Write("Interest Paid: {0}\t", (myPrincipal * myMonthlyInterestRate).ToString("$###,##0.00"));
+                                myPrincipal += myPrincipal * myMonthlyInterestRate;
+
+                                //{variable,xcolumnwidth:pattern}
+                                // in this case the variable is myPrincipal, x column width is 15 spaces,
+                                // positive value is right alligned, negative value is left alligned.
+                                // c stands for currency with a $ sign
+                                Console.Write($"Closing: {myPrincipal,15:c}\n");
+
+                            }
+
+                            Console.WriteLine($"Closing {myPrincipal.ToString("c"),20}");
+                            break;
+                        }
+                    case "X":
+                        {
+                            Console.WriteLine("\nThank you. Good bye.\n");
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("\nYour input for the menu choice is invalid. Try again.\n");
+                            break;
+                        }
+                }
+
+            } while (menuChoice.ToLower() != "x");
+            string msg = "Good luck on your future investments.";
+            for(int i = 0; i < msg.Length; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine($"\n{msg}\n");
+            for (int i = 0; i < msg.Length; i++)
+            {
+                Console.Write("*");
             }
         }
     }
