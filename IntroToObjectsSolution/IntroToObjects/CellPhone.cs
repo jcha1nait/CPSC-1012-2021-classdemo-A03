5d01a41708944a0ec95b623cfcb6906cb16aaa81
jcha1nait <77072999+jcha1nait@users.noreply.github.com>
2021-03-19 11:39:58 -0600

    IntroToObjects Program

---
+++ IntroToObjectsSolution/IntroToObjects/CellPhone.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntroToObjects
+{
+    class CellPhone
+    {
+        //Data Members
+
+        //Properties
+        //both fully and auto implemented properties
+
+        //Constructors
+
+        //Behaviour
+    }
+}
