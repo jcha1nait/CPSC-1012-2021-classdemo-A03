5ea34ada7315bace273d630f88f96cce23bc2874
jcha1nait <77072999+jcha1nait@users.noreply.github.com>
2021-03-29 12:25:15 -0600

    CollectionDemo Program

--- CollectionDemoSolution/CollectionDemo/Program.cs
+++ CollectionDemoSolution/CollectionDemo/Program.cs
@@ -9,11 +9,21 @@ using static System.Console; //needed for WriteLine, ReadLine, Write
 namespace CollectionDemo
 {
     class Program
-    {
+    {   
+        //entry point into your console application software
         static void Main(string[] args)
         {
             //BasicsOfLists();
 
+            //using composite classes
+            //class Room
+            //  within the class: Wall, Opening, List<T>
+
+            //create a collection of Walls for the room
+            //when the List<T> is created, the instance will be empty (count = 0)
+            List<Wall> Walls = new List<Wall>();
+            Walls = InputWallsForRoom();
+            List<Opening> Openings = new List<Opening>();
         }
 
         static void BasicsOfLists()
@@ -59,5 +69,87 @@ namespace CollectionDemo
             //removing from a list<T>
             setOfNumbers.Remove(number + 1);
         }
+
+        static List<Wall> InputWallsForRoom()
+        {   
+            //declare a variable capable of holding an instance of
+            //  the class Wall;
+            Wall aWall = null;
+            //you need to create a local List<T> BECAUSE
+            //  you DID NOT pass in the collection to be filled
+            //you will return the local List<T>
+            List<Wall> inputWalls = new List<Wall>();
+            bool finished = false;
+            do
+            {   
+                //to create an instance of the class Wall, use the
+                //  new operator and the class name.
+                aWall = new Wall();
+                //obtain the width for the wall
+                aWall.Width = GetPositiveDouble("Enter the width of your wall:");
+                //obtain the height for the wall
+                aWall.Height = GetPositiveDouble("Enter the height of your wall:");
+                //add the instance to the wall collection
+                inputWalls.Add(aWall);
+                if (GetNonEmptyString("Do you have another wall(y/n):").ToUpper() == ("N"))
+                {
+                    finished = true;
+                }
+            } while (!finished);
+            return inputWalls;
+        }
+
+        static double GetPositiveDouble(string prompt)
+        {
+            double number = 0;
+            string inputTemp = null;
+            bool valid = false;
+            do
+            {
+                Console.Write($"{prompt}\t");
+                inputTemp = Console.ReadLine();
+                if (double.TryParse(inputTemp,out number))
+                {
+                    if (number > 0)
+                    {
+                        valid = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("That is not a valid input(must be greater than 0). Try again.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Input is invalid {inputTemp}. Try again.");
+                }
+            } while (!valid);
+
+            return number;
+        }
+
+        static string GetNonEmptyString(string prompt)
+        {
+            string inputTempLocal;
+            bool valid = false;
+
+            do
+            {
+                Console.Write($"\n{prompt}");
+                inputTempLocal = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(inputTempLocal))
+                {
+                    valid = true;
+                }
+                else
+                {
+                    Console.WriteLine("\nThat is not a valid input(can not be empty). Try again.");
+                }
+            } while (!valid);
+
+            return inputTempLocal;
+        }
+
     }
 }
